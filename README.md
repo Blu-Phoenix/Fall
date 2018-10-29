3b35488232cddfd809480a7baea7f27f11b2079c
Blu-Phoenix <shawnroyer8@gmail.com>
2018-10-29 17:41:00 -0500

    Second Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 GITting some practice
 Michael Royer
+This is the first sentence. This is the second sentence. ;-P I'm not much for writing.
--- fall.py
+++ fall.py
@@ -1,2 +1,5 @@
 print ("Hello World!")
-print ("Fall is Falling Near")
+print ("Fall is Falling Near")\
+
+for i in range(12):
+	print ("This will print 12 times.")
