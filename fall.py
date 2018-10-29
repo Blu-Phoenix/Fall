21bd29a5889e2079d1781073888886f347a39c6b
Blu-Phoenix <shawnroyer8@gmail.com>
2018-10-29 17:51:26 -0500

    Third Commit

--- fall.py
+++ fall.py
@@ -3,3 +3,9 @@ print ("Fall is Falling Near")\
 
 for i in range(12):
 	print ("This will print 12 times.")
+
+var = "five"
+
+var = 5
+
+print ("I really like python, simply because it is so fast add building prototypes.")
